b7c5c87d068581470251c38bdc3d9c0752fd41ce
Jinpeng Yu <jinpeng.yu.april@outlook.com>
2024-10-20 00:10:36 +1100

    Update mask_predictor.py

---
+++ model/mask_predictor.py
@@ -0,0 +1,49 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class MaskPredictor(nn.Module):
+    def __init__(self, in_feat_dim, factor=2, is_last=False):
+        """
+        Initializes the MaskPredictor module.
+
+        Args:
+            in_feat_dim (int): The input feature dimension.
+            factor (int): Factor to reduce the hidden size. Defaults to 2.
+            is_last (bool): Indicates whether this is the last mask predictor in the sequence.
+                            If True, the output will have 2 channels (for binary mask prediction).
+        """
+        super(MaskPredictor, self).__init__()
+
+        hidden_size = in_feat_dim // factor
+
+        self.seq = nn.Sequential(
+            nn.Conv2d(hidden_size + in_feat_dim, hidden_size, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(hidden_size),
+            nn.ReLU(),
+            nn.Conv2d(hidden_size, hidden_size, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(hidden_size),
+            nn.ReLU()
+        )
+        self.conv = nn.Conv2d(hidden_size, 2, 1) if is_last else nn.Identity()
+
+    def forward(self, x, f):
+        """
+        Forward pass for the MaskPredictor module.
+
+        Args:
+            x (B, C, H, W): Input tensor.
+            f (B, C', H', W'): Feature tensor to concatenate with x.
+
+        Returns:
+            torch.Tensor: Output tensor after processing, shape depends on is_last:
+                          - If is_last is True, shape will be (B, 2, H'', W'').
+                          - If is_last is False, shape will be (B, hidden_size, H'', W'').
+        """
+        if x.size(-2) < f.size(-2) or x.size(-1) < f.size(-1):
+            x = F.interpolate(input=x, size=(f.size(-2), f.size(-1)), mode='bilinear', align_corners=True)
+        x = torch.cat([x, f], dim=1)
+        x = self.seq(x)
+        x = self.conv(x)
+        return x
