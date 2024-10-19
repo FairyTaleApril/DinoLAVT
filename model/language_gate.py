332ef0841ed813e787f3d335816a645139cf3886
Jinpeng Yu <jinpeng.yu.april@outlook.com>
2024-10-19 22:41:41 +1100

    Update language_gate.py

--- model/language_gate.py
+++ model/language_gate.py
@@ -5,11 +5,11 @@ class LanguageGate(nn.Module):
     def __init__(self, dim):
         super(LanguageGate, self).__init__()
 
-        self.res_gate = nn.Sequential(
+        self.lang_gate = nn.Sequential(
             nn.Linear(dim, dim, bias=False),
             nn.ReLU(),
             nn.Linear(dim, dim, bias=False),
             nn.Tanh())
 
     def forward(self, x, x_residual):
-        return x + (self.res_gate(x_residual) * x_residual)
+        return x + (self.lang_gate(x_residual) * x_residual)
